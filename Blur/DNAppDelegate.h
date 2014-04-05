03e1d5b0ed3c6a235bfb4fb1007b5d915686fd6c
Andrew Pouliot <andrew@darknoon.com>
2014-04-05 15:13:17 -0700

    Initial Commit

---
+++ Blur/DNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DNAppDelegate.h
+//  Blur
+//
+//  Created by Andrew Pouliot on 4/5/14.
+//  Copyright (c) 2014 Darknoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Blur/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Blur
+//
+//  Created by Andrew Pouliot on 4/5/14.
+//  Copyright (c) 2014 Darknoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DNAppDelegate class]));
+	}
+}
